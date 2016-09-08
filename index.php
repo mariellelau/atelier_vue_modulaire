a22dea8256374a91f918927360098feaf5124232
Marielle Lautrou <marielle.lautrou@gmail.com>
2016-09-08 11:34:20 +0200

    Modif site
    Ajout de la barre de navigation

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang=fr>
+<head>
+    <meta charset="UTF-8">
+    <title>Atelier vue modulaire</title>
+    
+     <!-- Compiled and minified CSS -->
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+</head>
+
+<body>
+	<header>
+		<nav>
+    		<div class="nav-wrapper">
+      			<a href="#" class="brand-logo">WCS</a>
+    			<ul id="nav-mobile" class="right hide-on-med-and-down">
+        			<li><a href="#">Images</a></li>
+        			<li><a href="#">About</a></li>
+       				<li><a href="#">Contact</a></li>
+      			</ul>
+    		</div>
+  		</nav>
+	</header>
+
+	<div class="container">
+		<div class="row">
+			<div class="col s12 m6 center">
+				<p>Hello World</p>
+			</div>
+			<div class="col s12 m6 center">
+				<p>Hello France</p>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Appel Script -->
+	<script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+
+  	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+</body>
+</html>
