6e50c66f38cda04ad45ad56a30162e413eb1a4ed
Marielle Lautrou <marielle.lautrou@gmail.com>
2016-09-08 14:39:51 +0200

    Test SSH

---
+++ include/footer.php
@@ -0,0 +1,14 @@
+	<!-- Appel Script -->
+<script
+			  src="https://code.jquery.com/jquery-2.2.4.js"
+			  integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
+			  crossorigin="anonymous"></script>
+
+  	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+
+	<!--Caroussel -->
+	<script type="text/javascript">$('.carousel.carousel-slider').carousel({full_width: true}); 
+</script>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang=fr>
+<head>
+    <meta charset="UTF-8">
+    <title>Atelier vue modulaire</title>
+    
+     <!-- Compiled and minified CSS -->
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+</head>
+
+<body>
+	<header>
+		<nav>
+    		<div class="nav-wrapper">
+      			<a href="#" class="brand-logo">WCS</a>
+    			<ul id="nav-mobile" class="right hide-on-med-and-down">
+        			<li><a href="images.php">Images</a></li>
+        			<li><a href="#">About</a></li>
+       				<li><a href="#">Contact</a></li>
+      			</ul>
+    		</div>
+  		</nav>
+	</header>
+</body>
+</html>
